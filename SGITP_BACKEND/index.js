d5c1968af57757cb3bd267e0da05406df90f2917
AAlejandroDd <20240657@ricaldone.edu.sv>
2026-05-25 21:44:38 -0600

    Configuracion inicial para la Api, creacion de carpetas del backend y los archivos para la configuracion de esta. Instalacion de Dependencias.

---
+++ SGITP_BACKEND/index.js
@@ -0,0 +1,12 @@
+import app from "./app.js"
+import "./database.js"
+import { config } from "./src/config.js";
+
+//Creamos la función 
+//Que se encarga de ejecutar el servidor
+async function main() {
+    app.listen(config.server.port)
+    console.log("server on port"+ config.server.port)
+}
+
+main();
